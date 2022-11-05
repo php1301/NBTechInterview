e5cb1340b411374780370896ad63d9051477147f
php1301 <19520854@gm.uit.edu.vn>
2022-11-05 23:10:44 +0700

    Add Authentication and configure Linter, Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
-import "@styles/global.scss";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { DehydratedState, QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+import "tailwindcss/tailwind.css";
+import "@styles/global.scss";
+
+function MyApp({
+    Component,
+    pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>): JSX.Element {
     const queryClient = new QueryClient();
     return (
         <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <Component {...pageProps} />
-            </Hydrate>
+            <UserProvider>
+                <Hydrate state={pageProps.dehydratedState}>
+                    <Component {...pageProps} />
+                </Hydrate>
+            </UserProvider>
         </QueryClientProvider>
     );
 }
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 import { Button } from "@components";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export const Main: React.FC = () => {
+    const { user, error, isLoading } = useUser();
+
+    // you can use the error and loading state to show an error message or a loading spinner while loading.
+    if (isLoading) {
+        return (
+            <div className="text-5xl font-semibold text-center text-indigo-600">
+                ...loading{" "}
+            </div>
+        );
+    }
+
+    // if (error) {
+    //     return (
+    //         <div className="text-5xl font-semibold text-center text-indigo-600">
+    //             {error.message}
+    //         </div>
+    //     );
+    // }
     return (
         <div className="text-center font-light py-5 bg-gray-700">
             <div className="container mx-auto">
@@ -10,7 +29,7 @@ export const Main: React.FC = () => {
                     data-test="main-heading"
                     className="text-white text-8xl mb-2"
                 >
-                    Nimble Tech Interview
+                    Nimble Tech Interview For {user?.email || ''}
                 </h1>
                 <p className="text-lg text-white mb-3">
                     Technical Test for Nimble
